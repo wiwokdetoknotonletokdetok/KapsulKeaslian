dc6cf015a2a8ba5e285aad659afc9cd6f1eb7e51
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-06-25 01:03:24 +0700

    Initialize springboot

---
+++ src/test/java/org/wiwokdetok/kapsulkeaslian/KapsulKeaslianApplicationTests.java
@@ -0,0 +1,12 @@
+package org.wiwokdetok.kapsulkeaslian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapsulKeaslianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
