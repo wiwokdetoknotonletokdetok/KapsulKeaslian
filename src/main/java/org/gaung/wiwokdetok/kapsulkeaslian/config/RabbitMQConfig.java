76afa17cccf26231ec6f0626ffcb212851bf9565
kaachiii <kaachi579@gmail.com>
2025-07-06 00:12:54 +0700

    update config rabbitmq (lagi)

--- src/main/java/org/gaung/wiwokdetok/kapsulkeaslian/config/RabbitMQConfig.java
+++ src/main/java/org/gaung/wiwokdetok/kapsulkeaslian/config/RabbitMQConfig.java
@@ -1,11 +1,17 @@
 package org.gaung.wiwokdetok.kapsulkeaslian.config;
 
+import org.gaung.wiwokdetok.kapsulkeaslian.dto.UserPointMessage;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RabbitMQConfig {
@@ -14,12 +20,34 @@ public class RabbitMQConfig {
     public static final String EXCHANGE_NAME = "book.exchange";
 
     @Bean
-    public Queue userPointsQueue() {
-        return new Queue(QUEUE_USER_POINTS);
+    public TopicExchange bookExchange() {
+        return new TopicExchange(EXCHANGE_NAME, true, false);
     }
 
     @Bean
-    public Binding userPointsBinding(Queue userPointsQueue) {
-        return BindingBuilder.bind(userPointsQueue).to(new TopicExchange(EXCHANGE_NAME)).with(ROUTING_KEY_USER_POINTS);
+    public Queue userPointsQueue() {
+        return new Queue(QUEUE_USER_POINTS, true);
     }
+
+    @Bean
+    public Binding userPointsBinding(Queue userPointsQueue, TopicExchange bookExchange) {
+        return BindingBuilder
+                .bind(userPointsQueue)
+                .to(bookExchange)
+                .with(ROUTING_KEY_USER_POINTS);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter consumerJackson2MessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+
+        Map<String, Class<?>> typeIdMappings = new HashMap<>();
+        typeIdMappings.put("org.gaung.wiwokdetok.fondasikehidupan.dto.UserPointMessage", UserPointMessage.class);
+        converter.setClassMapper(new DefaultClassMapper() {{
+            setIdClassMapping(typeIdMappings);
+        }});
+
+        return converter;
+    }
+
 }
