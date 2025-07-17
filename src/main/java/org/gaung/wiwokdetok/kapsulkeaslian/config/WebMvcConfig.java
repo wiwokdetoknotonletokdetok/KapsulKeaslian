f2af8cc326c7223993103307b700990823a7918f
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-07-17 16:32:38 +0700

    [CONFIG] Allowed origins and headers

--- src/main/java/org/gaung/wiwokdetok/kapsulkeaslian/config/WebMvcConfig.java
+++ src/main/java/org/gaung/wiwokdetok/kapsulkeaslian/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package org.gaung.wiwokdetok.kapsulkeaslian.config;
 
 import org.gaung.wiwokdetok.kapsulkeaslian.security.resolver.CurrentUserResolver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,6 +11,12 @@ import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Value("${cors.allowed-headers}")
+    private String[] allowedHeaders;
 
     private final CurrentUserResolver currentUserResolver;
 
@@ -25,8 +32,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders(allowedHeaders);
     }
 }
